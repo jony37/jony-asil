840ad1b3c4e18ee9f26221fd516b613b14dd6b10
jony <ravshanovjony7@gmail.com>
2025-06-22 13:53:03 +0500

    project components

---
+++ src/components/app/App.js
@@ -0,0 +1,19 @@
+import Header from "../header/header";
+import Home from "../home/home";
+import Shop from "../shop/shop";
+import Product from "../product/product";
+import Footer from "../footer/footer";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Home/>
+      <Shop/>
+      <Product/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
